da5e78d337f0eabec789e1771d88b99b93d41f55
Harmon Wood <harmonwood@gmail.com>
2019-11-15 20:59:01 -0600

    Created Windows Subsystem Linux installer

---
+++ README.md
@@ -0,0 +1,21 @@
+## Install
+
+* Open Powershell using `Run As Adminstrator`
+* Download script via `Invoke-WebRequest -Uri https://github.com/harmonwood/wsl-installer/src/install.ps1 -OutFile .\install.ps1 -UseBasicParsing`
+* Invoke with `powershell -ExecutionPolicy ByPass -File .\install.ps1`
+* Reboot when requested
+* ReInvoke script after first reboot
+
+# Starting Options
+After insalled you can run the POSIX Shell at any time in the following ways:
+* The Windows App Bar and select your chosen distribution (Ubuntu by default)
+* Executing the given binary  for your chosen distribution` (ubuntu1840.exe`)
+* This scripts has also added a generic symbolic link no matter the chosen distribution (`posix.exe`)
+
+# Changing the distribution
+If you do not like ubuntu and want to use another distribution you will need to change the following variables in the script before running it.
+* `$distroName = 'Ubuntu 18.04'`
+* `$distroExe = 'ubuntu1804.exe'`
+* `$distroUrl = 'https://aka.ms/wsl-ubuntu-1804'`
+
+A complete list of options and distroUrls can be found at `https://docs.microsoft.com/en-us/windows/wsl/install-manual`
---
+++ src/install.ps1
@@ -0,0 +1,41 @@
+﻿# Run as Adminstrator!
+# Temporary fix: `powershell -ExecutionPolicy ByPass -File .\development.ps1`
+# Permenant Fix: `Set-ExecutionPolicy unrestricted` 
+
+#Properties
+# Distros (https://docs.microsoft.com/en-us/windows/wsl/install-manual)
+$distroName = 'Ubuntu 18.04'
+$distroExe = 'ubuntu1804.exe'
+$distroUrl = 'https://aka.ms/wsl-ubuntu-1804'
+
+# Enable Linux Subsystem
+$subsystem = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+if ($subsystem.State -eq "Disabled")
+{
+	Write-Host '[Subsystems] Enabeling Subsystems'
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+	# TODO: Restart script after reboot
+}
+
+# Download Linux distro
+Write-Host '[Subsystems] Dowloading' $distroName '...'
+Invoke-WebRequest -Uri $distroUrl -OutFile C:\Users\Public\posix.appx -UseBasicParsing
+Write-Host '[Subsystems] Download Complete!'
+
+# Install Linux Distro
+Write-Host '[Subsystems] Starting installation...'
+Add-AppxPackage C:\Users\Public\posix.appx
+Write-Host '[Subsystems] Install Complete!'
+
+# Everything after this is just to allow for the system to be started from the cmd
+Write-Host '[Subsystems] Configuring environement...'
+Rename-Item C:\Users\Public\posix.appx C:\Users\Public\posix.zip
+Expand-Archive C:\Users\Public\posix.zip C:\Users\Public\distro
+New-Item -ItemType SymbolicLink -Path C:\Users\Public\distro\posix.exe -Target C:\Users\Public\distro\$distroExe
+$userenv = [System.Environment]::GetEnvironmentVariable("Path", "User")
+[System.Environment]::SetEnvironmentVariable("PATH", $userenv + ";C:\Users\Public\distro", "User")
+Write-Host '[Subsystems] Envirnoment Configured!'
+
+# First run
+Write-Host 'First run...'
+C:\Users\Public\distro\posix.exe
